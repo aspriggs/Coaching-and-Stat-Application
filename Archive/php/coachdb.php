a73ddf740386cc3e0e53e623cbccad41affdcff2
Anthony Spriggs <antspriggs31@aol.com>
2016-04-21 14:00:04 -0400

    Delete coachdb.php

--- Archive/php/coachdb.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//Store database access information
-$host = "localhost";$user = "aspriggs";$password = "King703!";$database = "littleleague";
-
-//Connect to MySQL Database
-$coachDB = new mysqli($host, $user, $password, $database);
-
-?>
